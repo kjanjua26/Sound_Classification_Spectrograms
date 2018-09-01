4648fa6d567b11071d059c4a1e79e5f7046b0bca
Kamran Janjua <kamranejaz98@gmail.com>
2018-09-01 02:00:07 +0200

    Update convert_sound_spec.py

--- utils/convert_sound_spec.py
+++ utils/convert_sound_spec.py
@@ -8,9 +8,8 @@ dataset_ = '/Users/Janjua/Desktop/ESC-50-master/dataset/'
 
 def graph_spectrogram(wav_file, class_, filename):
     sound_info, frame_rate = get_wav_info(wav_file)
-    pylab.figure(num=None, figsize=(19, 12))
+    pylab.figure(num=None, figsize=(256, 256))
     pylab.subplot(111)
-    pylab.title('spectrogram of %r' % wav_file)
     pylab.specgram(sound_info, Fs=frame_rate)
     pylab.savefig('/Users/Janjua/Desktop/ESC-50-master/dataset/{}/{}.png'.format(class_, filename))
 
